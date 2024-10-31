4be4d033ea7c7362f86c419011913caee6fa56be
Rodzell Tucker <tuckerr8@mail.uc.edu>
2024-10-31 12:33:44 -0400

    Add project files.

---
+++ WebAPIGit_Tucker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAPIGit_Tucker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
